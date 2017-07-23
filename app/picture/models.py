e971330fdf4e3a848efba64d782800391c92e2e0
bayi27 <328588917@qq.com>
2017-07-23 19:54:33 +0800

    no message

---
+++ app/picture/models.py
@@ -0,0 +1,19 @@
+from app.extensions import db
+
+
+class UploadPic(db.Model):
+    """UploadPic model """
+    id=db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+
+    def to_json2(self):
+        """Returns a json representantion of the user.
+        :returns: a json object.
+
+        """
+
+        return {
+            'id': str(self.id),
+            'username': self.username
+        }
