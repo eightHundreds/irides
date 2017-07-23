e53931aeca8ed7e553ebaf5e4f4c74f2f8263d70
bayi27 <328588917@qq.com>
2017-07-23 20:07:31 +0800

    no message

--- app/tags/models.py
+++ app/tags/models.py
@@ -3,17 +3,3 @@ from app.extensions import db
 
 class PicTags(db.Model):
     """PicTags model """
-    id=db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
-
-    def to_json2(self):
-        """Returns a json representantion of the user.
-        :returns: a json object.
-
-        """
-
-        return {
-            'id': str(self.id),
-            'username': self.username
-        }
